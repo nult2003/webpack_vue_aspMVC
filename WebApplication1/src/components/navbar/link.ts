b763f0c841738a4836dcffc7e047843e07e569f4
Minh Nhat <nult2003@gmail.com>
2019-03-14 17:48:58 +0700

    Add project
    webpack+vue+asp net mvc

---
+++ WebApplication1/src/components/navbar/link.ts
@@ -0,0 +1,11 @@
+﻿import { subsite } from "../../util/helpers";
+
+export class Link {
+    name: string;
+    path: string;
+
+    constructor(name: string, path: string) {
+        this.name = name;
+        this.path = subsite(path);
+    }
+}
